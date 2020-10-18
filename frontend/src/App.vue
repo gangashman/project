7d603758e195edbd349e820faa6e4e50d3902d6a
gangashman <ganagsh@gmail.com>
2020-10-18 19:53:09 +0300

    auth dialog

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,9 +8,65 @@
 
         <v-spacer></v-spacer>
 
+        <v-btn color="primary" dark class="ma-2" @click="dialog_login = !dialog_login">
+          Open Dialog 2
+        </v-btn>
+        <v-dialog v-model="dialog_login" max-width="500px" :persistent="auth_loading">
+          <v-list three-line subheader>
+            <v-list-item>
+              <v-list-item-content>
+                <v-tabs v-model="auth_tab">
+                  <v-tab :disabled="auth_loading">Login</v-tab>
+                  <v-tab :disabled="auth_loading">Registration</v-tab>
+
+                  <v-tab-item>
+                    <v-text-field
+                      label="Login" :rules="rules" hide-details="auto"
+                      :disabled="auth_loading"></v-text-field>
+                    <v-text-field
+                      label="Password" type="password" :rules="rules" hide-details="auto"
+                      :disabled="auth_loading"></v-text-field>
+                    <v-col class="text-right">
+                      <v-btn
+                        class="ma-2" :loading="auth_loading" :disabled="auth_loading"
+                        color="primary" @click="auth_loading = 'loading'">
+                        Login
+                      </v-btn>
+                    </v-col>
+                    <v-divider class="my-2"></v-divider>
+                    <v-btn depressed>
+                      Normal
+                      <v-icon right><GoogleSVG/></v-icon>
+                    </v-btn>
+                  </v-tab-item>
+                  <v-tab-item>
+                    <v-text-field
+                      label="Login" :rules="rules" hide-details="auto"
+                      :disabled="auth_loading"></v-text-field>
+                    <v-text-field
+                      label="Email" :rules="rules" hide-details="auto"
+                      :disabled="auth_loading"></v-text-field>
+                    <v-text-field
+                      label="Password" type="password" :rules="rules" hide-details="auto"
+                      :disabled="auth_loading"></v-text-field>
+                    <v-col class="text-right">
+                      <v-btn
+                        class="ma-2" :loading="auth_loading" :disabled="auth_loading"
+                        color="primary" @click="auth_loading = 'loading'">
+                        Register
+                      </v-btn>
+                    </v-col>
+                  </v-tab-item>
+                </v-tabs>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-dialog>
+        
         <v-responsive max-width="260">
           <v-text-field dense flat hide-details rounded solo-inverted></v-text-field>
         </v-responsive>
+
       </v-container>
     </v-app-bar>
 
@@ -52,12 +108,19 @@
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      links: [
-        { title: 'Home', url: '/', icon: 'mdi-home' },
-        { title: 'About', url: '/about',icon: 'mdi-help-box' },
-      ]
-    }),
-  }
+ import GoogleSVG from "@/assets/logo-google.svg";
+ export default {
+   components: {
+     GoogleSVG
+   },
+   data: () => ({
+     dialog_login: false,
+     auth_loading: false,
+     auth_tab: null,
+     links: [
+       { title: 'Home', url: '/', icon: 'mdi-home' },
+       { title: 'About', url: '/about',icon: 'mdi-help-box' },
+     ]
+   }),
+ }
 </script>
