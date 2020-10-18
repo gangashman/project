3e007fc43211f1f7a7258f8f144a9d011d373a69
gangashman <ganagsh@gmail.com>
2020-10-18 14:38:59 +0300

    title views

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,5 @@
 import { createApp } from 'vue'
 import App from './components/App.vue'
 import router from './router'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
 
 createApp(App).use(router).mount('#app')
